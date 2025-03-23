06c1c45444fae0d88518642f229f707c1082bd3c
ZhBauyrzhan <bauyrzhanzhonkebayev@Bauyrzhans-MacBook-Air.local>
2025-03-23 21:09:43 +0500

    flash

--- lua/zhnb/init.lua
+++ lua/zhnb/init.lua
@@ -2,4 +2,56 @@ require("zhnb.set")
 require("zhnb.remap")
 require("zhnb.lazy_init")
 vim.cmd.colorscheme("gruvbox")
+local augroup = vim.api.nvim_create_augroup
+local zhnb = augroup('zhnb', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+vim.filetype.add({
+    extension = {
+        templ = 'templ',
+    }
+})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = zhnb,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+
+autocmd('LspAttach', {
+    group = zhnb,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-r>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
 
